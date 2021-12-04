e9f7bc8f094fbe277568658e808595728dfd89c2
AnnelyKalmus <annelykalmus@icloud.com>
2021-12-04 14:01:14 +0200

    [add ] - category controller in progress

---
+++ src/main/java/com/example/endofgame/dto/CategorySummary.java
@@ -0,0 +1,10 @@
+package com.example.endofgame.dto;
+
+import java.time.LocalDateTime;
+
+public record CategorySummary(Long id, String name,
+                              LocalDateTime created,
+                              LocalDateTime updated) {
+
+
+}
---
+++ src/main/java/com/example/endofgame/entity/Category.java
@@ -0,0 +1,29 @@
+package com.example.endofgame.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "CATEGORIES")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime creationTimestamp;
+
+    private LocalDateTime updateTimestamp;
+
+
+}
