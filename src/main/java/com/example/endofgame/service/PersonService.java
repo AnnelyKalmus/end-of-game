3b45c99a031d64d16558865ce702e8488211a6ba
AnnelyKalmus <annelykalmus@icloud.com>
2021-12-04 10:20:52 +0200

    [add ] - threads exercises

--- src/main/java/com/example/endofgame/service/PersonService.java
+++ src/main/java/com/example/endofgame/service/PersonService.java
@@ -6,6 +6,8 @@ import com.example.endofgame.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+
 @Service
 @Slf4j
 public class PersonService {
@@ -14,13 +16,47 @@ public class PersonService {
 
     private final PersonConverter personConverter;
 
-    public PersonService(final PersonRepository repository, final PersonConverter personConverter) {
+private final ExecutorService workers;
+
+    public PersonService(final PersonRepository repository, final PersonConverter personConverter, final ExecutorService workers) {
         this.repository = repository;
         this.personConverter = personConverter;
+        this.workers = workers;
     }
 
     public PersonSummary getMyPerson() {
         log.info("getMyPerson()");
         return personConverter.fromEntityToDto(repository.findMe());
+    }
+
+    public void runNewThread() {
+        // Executor ThreadExecutor
+        // Thread vs Runnable
+        // Thread is some kind of worker
+        // Runnable is just job for worker
+        //Thread pool is set of workers ready to take any job (Runnable)
+
+Runnable anotherJob = new Runnable() {
+    @Override
+    public void run() {
+   log.info("inside anonymous class");
+   log.info("I'm running by: [" + Thread.currentThread().getName() + "]");
+    }
+};
+
+Runnable newJob = () -> {
+    log.info("inside lambda");
+    log.info("I'm running by: [" + Thread.currentThread().getName() + "]");
+    try {
+        Thread.sleep(2000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+};
+
+workers.submit(newJob);
+workers.submit(anotherJob);
+
+
     }
 }
